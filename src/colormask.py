c492fc0c49dcbab8fffdde6cfc70b2b1e68a3d1b
Branden Evangelista <bevangelista@ufl.edu>
2022-09-17 12:12:33 -0700

    Added a main method

--- src/colormask.py
+++ src/colormask.py
@@ -3,17 +3,25 @@ import numpy as np
 from matplotlib import pyplot as plt
 import constants
 
-#Load camera frame
-video = cv2.VideoCapture(constants.WEBCAM)
+#function to generate video feed into window
+def loadVideo(webcamVersion):
+    #Load camera frame
+    video = cv2.VideoCapture(webcamVersion)
 
-while True:
-    ret, frame = video.read()
-    cv2.imshow('frame', frame)
+    while True:
+        ret, frame = video.read()
+        cv2.imshow('frame', frame)
 
-    #Press q to end video transmission and close window
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        #Press q to end video transmission and close window
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-#Release video capture object and close all windows
-video.release()
-cv2.destroyAllWindows()
+    #Release video capture object and close all windows
+    video.release()
+    cv2.destroyAllWindows()
+
+def main():
+    loadVideo(constants.WEBCAM)
+
+if __name__ == '__main__':
+    main()
