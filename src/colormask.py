9b2756025fb0e73c8ab5b29af2800402fe3dbb1a
Branden Evangelista <bevangelista@ufl.edu>
2022-09-17 12:09:41 -0700

    Added way to pull video feed to python + added constants file + added gitignore

--- src/colormask.py
+++ src/colormask.py
@@ -1,1 +1,19 @@
-print("hello world")
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import constants
+
+#Load camera frame
+video = cv2.VideoCapture(constants.WEBCAM)
+
+while True:
+    ret, frame = video.read()
+    cv2.imshow('frame', frame)
+
+    #Press q to end video transmission and close window
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#Release video capture object and close all windows
+video.release()
+cv2.destroyAllWindows()
