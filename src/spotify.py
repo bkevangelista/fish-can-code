296b3ea6d42790c1c79d5a935f14174c766f0095
Branden Evangelista <bevangelista@ufl.edu>
2022-09-18 19:50:39 -0700

    Reformat data + pull data from various users' playlists

--- src/spotify.py
+++ src/spotify.py
@@ -54,55 +54,31 @@ def getSongInformation(searchObject):
     return songInfo
 
 #Initialize connection to Spotify
-config_object = ConfigParser()
-config_object.read('src/secrets.ini')
-spotifyCredentials = config_object["SPOTIFY"]
-clientID = spotifyCredentials["CLIENT_ID"]
-clientSecret = spotifyCredentials["CLIENT_SECRET"]
-uri = spotifyCredentials["URI"]
+def initializeSpotifyConnection():
+    config_object = ConfigParser()
+    config_object.read('src/secrets.ini')
+    spotifyCredentials = config_object["SPOTIFY"]
+    clientID = spotifyCredentials["CLIENT_ID"]
+    clientSecret = spotifyCredentials["CLIENT_SECRET"]
+    uri = spotifyCredentials["URI"]
 
-sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth
-                                (client_id=clientID,
-                                client_secret=clientSecret,
-                                redirect_uri=uri,
-                                scope="user-library-read playlist-read-private"))
+    return spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth
+                                    (client_id=clientID,
+                                    client_secret=clientSecret,
+                                    redirect_uri=uri,
+                                    scope="user-library-read playlist-read-private app-remote-control playlist-modify-public"))
+
+sp = initializeSpotifyConnection()
 username = sp.me()['id']
-kaitlinPlaylists = listAllPlaylists(sp, 'kate.sm')
-print(json.dumps(kaitlinPlaylists, sort_keys=True, indent=4))
 
-tracks = getSongsFromPlaylist(sp, 5, 0, kaitlinPlaylists[5]["playlistID"])
+allPlaylists = []
+allPlaylists.append(listAllPlaylists(sp, username))
+allPlaylists.append(listAllPlaylists(sp, '12155045153'))
+allPlaylists.append(listAllPlaylists(sp, 'kate.sm'))
+allPlaylists.append(listAllPlaylists(sp, 'saxguitarguy'))
+allPlaylists.append(listAllPlaylists(sp, '22ag7odizaakrf6vgghlut6ni')) #this is Mel
+allPlaylists.append(listAllPlaylists(sp, 'galamares12'))
+
+print(json.dumps(allPlaylists[4], indent=4))
+tracks = getSongsFromPlaylist(sp, 5, 0, allPlaylists[4][0]["playlistID"])
 print(json.dumps(tracks, indent=4))
-
-
-# # Simple program that allows user to search a song and play it through the web browser
-# while True:
-#     print("Welcome")
-#     print("0 - Exit console")
-#     print("1 - Search for a song")
-#     user_input = int(input("Enter your choice: "))
-
-#     if user_input == 1:
-#         search_song = input("Enter song name: ")
-#         search_artist = input("Enter artist name: ")
-#         q = f'Track:{search_song} Artist:{search_artist}'
-#         results = spotifyObject.search(q, type="track", limit=1)
-
-#         songInfo = getSongInformation(results)
-#         print(json.dumps(songInfo, sort_keys=True, indent=4))
-#         webbrowser.open(songInfo['songUrl'])
-        
-#         # songs_dict = results['tracks']
-#         # song_items = songs_dict['items']
-
-#         # print(json.dumps(song_items, sort_keys=True, indent=4))
-#         # print('Album: ' + song_items[0]['album']['name'])
-#         # print('Song title: ' + song_items[0]['name'])
-#         # print('Artist name: ' + song_items[0]['album']['artists'][0]['name'])
-
-#         # song = song_items[0]['external_urls']['spotify']
-#         # webbrowser.open(song)
-#     elif user_input == 0:
-#         print("Exiting console")
-#         break
-#     else:
-#         print("Please enter a valid input")
