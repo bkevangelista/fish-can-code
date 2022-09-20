26d8b6e2de088546822d22c0abb246d52ef867e2
Branden Evangelista <bevangelista@ufl.edu>
2022-09-19 17:32:20 -0700

    Print all songs in a playlist

--- src/spotify.py
+++ src/spotify.py
@@ -12,13 +12,22 @@ def listAllPlaylists(spotifyObject, username):
             "playlistName": playlist['name'],
             "playlistID": playlist['id']
         })
-    return allPlaylists
+    
+    #Return dictionary of an array of playlists categorized by username
+    return {"username": username, "playlists": allPlaylists}
 
 #Very slow to go through all liked songs
-def getSongsFromPlaylist(spotifyObject, limit, offset, playlistID):
-    tracks = []
-    results = spotifyObject.playlist_tracks(playlist_id=playlistID, limit=limit, offset=offset)
-    for idx, item in enumerate(results['items']):
+def getSongsFromPlaylist(spotifyObject, playlistID):
+    allTracks = []
+    results = spotifyObject.playlist_tracks(playlist_id=playlistID)
+    tracks = results['items']
+
+    while results['next']:
+        #Go to the next set of songs
+        results = spotifyObject.next(results)
+        tracks.extend(results['items'])
+    
+    for idx, item in enumerate(tracks):
         track = item['track']
         allArtists = []
         for artist in track['artists']:
@@ -29,8 +38,8 @@ def getSongsFromPlaylist(spotifyObject, limit, offset, playlistID):
             "artists": allArtists,
             "album": str(track['album']['name'])
         }
-        tracks.append(trackInfo)
-    return tracks
+        allTracks.append(trackInfo)
+    return allTracks
     # for idx, item in enumerate(results['items']):
     #     track = item['track']
     #     print(idx+1, track['artists'][0]['name'], " – ", track['name'])
@@ -68,17 +77,26 @@ def initializeSpotifyConnection():
                                     redirect_uri=uri,
                                     scope="user-library-read playlist-read-private app-remote-control playlist-modify-public"))
 
-sp = initializeSpotifyConnection()
-username = sp.me()['id']
+def main():
+    sp = initializeSpotifyConnection()
+    username = sp.me()['id']
 
-allPlaylists = []
-allPlaylists.append(listAllPlaylists(sp, username))
-allPlaylists.append(listAllPlaylists(sp, '12155045153'))
-allPlaylists.append(listAllPlaylists(sp, 'kate.sm'))
-allPlaylists.append(listAllPlaylists(sp, 'saxguitarguy'))
-allPlaylists.append(listAllPlaylists(sp, '22ag7odizaakrf6vgghlut6ni')) #this is Mel
-allPlaylists.append(listAllPlaylists(sp, 'galamares12'))
+    allPlaylists = []
+    allPlaylists.append(listAllPlaylists(sp, username))
+    allPlaylists.append(listAllPlaylists(sp, '12155045153')) #this is James
+    allPlaylists.append(listAllPlaylists(sp, 'kate.sm'))
+    allPlaylists.append(listAllPlaylists(sp, 'saxguitarguy'))
+    allPlaylists.append(listAllPlaylists(sp, '22ag7odizaakrf6vgghlut6ni')) #this is Mel
+    allPlaylists.append(listAllPlaylists(sp, 'galamares12'))
 
-print(json.dumps(allPlaylists[4], indent=4))
-tracks = getSongsFromPlaylist(sp, 5, 0, allPlaylists[4][0]["playlistID"])
-print(json.dumps(tracks, indent=4))
+    # print(json.dumps(allPlaylists, indent=4))
+
+    tracks = getSongsFromPlaylist(sp, allPlaylists[0]['playlists'][0]["playlistID"])
+    print(len(tracks))
+    print(json.dumps(tracks, sort_keys=True, indent=4))
+
+    # tracks = getSongsFromPlaylist(sp, 5, 0, allPlaylists[0]['playlists'][0]["playlistID"])
+    # print(json.dumps(tracks, indent=4))
+
+if __name__ == '__main__':
+    main()
